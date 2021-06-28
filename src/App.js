56234711dbbc981c0f2a23f34adfd5d7ae3eafe1
webmastersmith <bryon@smithauto.us>
2021-06-27 20:36:33 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      string: 'Hello Bryon!!!'
+    }
+  }
+
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Bryon. 
-          I'm winner!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    )
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={() => this.setState({string: 'Hello Carrie'})}> Change Text </button>
+        </header>
+      </div>
+    );
   }
 }
+
 export default App;
